1e67a2a9ef716d2573e7114ae89d545d4cd6f314
edris shirzad <amk2015@AMKs-MacBook-Pro.local>
2025-05-15 14:11:22 -0400

    Set footer background to black

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 export default function Footer() {
   return (
-    <footer className="bg-slate-950 border-t border-slate-900 py-12">
+    <footer className="bg-black border-t border-neutral-800 py-12 text-white">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div className="md:col-span-2">
